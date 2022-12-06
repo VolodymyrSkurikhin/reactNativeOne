c11a675360a3136fe65f3bf1e1d82aa9d3a73e15
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-12-06 22:14:08 +0200

    start profile

--- screens/mainScreens/ProfileScreen.js
+++ screens/mainScreens/ProfileScreen.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { authSignOutUser } from "../../redux/auth/authOperations";
+import db from "../../firebase/config";
 
 const ProfileScreen = () => {
   const dispatch = useDispatch();
+  const { userId } = useSelector((state) => state.auth);
+
+  useEffect(() => {}, []);
+
+  const getUserPosts = async () => {
+    await db
+      .firestore()
+      .collection("posts")
+      .where("userId", "==", userId)
+      .onSnapshot((data) =>
+        console.log(data.docs.map((doc) => ({ ...doc.data() })))
+      );
+  };
 
   const signOut = () => dispatch(authSignOutUser());
 
